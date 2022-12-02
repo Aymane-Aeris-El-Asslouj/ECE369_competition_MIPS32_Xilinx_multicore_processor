0074242683ff50092b9372d4f191f1b208efb103
Aymane-Aeris-El-Asslouj <elassloujaymane@gmail.com>
2022-12-02 15:52:22 -0700

    tested on board and full working

--- sim_1/new/multicore_tb.v
+++ sim_1/new/multicore_tb.v
@@ -12,8 +12,8 @@ module multicore_tb();
     integer i =0;
 
 
-    wire [31:0] out_v0, out_v1, out_s0;
-    MultiCore t1(Clk, Reset, out_v0, out_v1, out_s0); //, buf_val_1_s, buf_val_2_s, buf_flag_s);
+    wire [31:0] out_v0, out_v1, out_s0, out_PC;
+    MultiCore t1(Clk, Reset, out_v0, out_v1, out_s0, out_PC); //, buf_val_1_s, buf_val_2_s, buf_flag_s);
 
 	initial begin
 		Clk <= 1'b0;
@@ -21,17 +21,6 @@ module multicore_tb();
 	end
 	
 	initial begin
-	   Reset <= 1;
-	   @(posedge Clk) #1;
-	   @(posedge Clk) #1;
-	   @(posedge Clk) #1;
-	   @(posedge Clk) #1;
-	   Reset <= 0;
-	   
-	   for(i =0; i < 500; i = i + 1) begin
-	       @(posedge Clk);
-	   end
-	   
 	   Reset <= 1;
 	   @(posedge Clk) #1;
 	   @(posedge Clk) #1;
